406753f3d1cac0aec23242456f0427098608e598
EKnight-Blue <loicchevalier@gmail.com>
2024-01-12 11:13:44 +0100

    pouissance avec **

--- tp2/traduction/Correction_Horloge_Satellite.py
+++ tp2/traduction/Correction_Horloge_Satellite.py
@@ -9,7 +9,7 @@ def Correction_Horloge_Satellite(t, Ephem):
 
     F = -4.442807633e-10   # Constantes qui dépendent de la gravité terrestre
     mu = 3.986005e14
-    a = Ephem(17)^2
+    a = Ephem(17) ** 2
     ecc = Ephem(3)
     af0 = Ephem(18)    # Correction horloge ordre 0
     af1 = Ephem(19)    # Correction horloge ordre 1
@@ -19,7 +19,7 @@ def Correction_Horloge_Satellite(t, Ephem):
     deltan = Ephem(10)
     M0 = Ephem(7)
 
-    n0 = (mu / a^3)^0.5
+    n0 = (mu / a ** 3) ** 0.5
 
     N = n0 + deltan
 
@@ -34,12 +34,12 @@ def Correction_Horloge_Satellite(t, Ephem):
 
     Ek = Mk
 
-    while abs(Mk - Ek + ecc * sin(Ek)) > 10^-12:  #boucle de calcul de Ek
+    while abs(Mk - Ek + ecc * sin(Ek)) > 10 ** -12:  #boucle de calcul de Ek
 
         Ek = Ek - (Ek - ecc * sin(Ek) - Mk) / (1 - ecc * cos(Ek))
 
-    DeltaR = F*ecc*a^0.5*sin(Ek) # Terme de correction relativiste
+    DeltaR = F*ecc*a ** 0.5 * sin(Ek) # Terme de correction relativiste
 
-    corPR = af0 + af1*(t-toc) + af2*(t-toc)^2 + DeltaR  
+    corPR = af0 + af1*(t-toc) + af2*(t-toc) ** 2 + DeltaR
 
     return corPR

20b021acb043256ac3288daed09c3678db94e809
EKnight-Blue <loicchevalier@gmail.com>
2024-01-12 12:11:07 +0100

    indice ephemeride

--- tp2/read_csv.py
+++ tp2/read_csv.py
@@ -32,7 +32,7 @@ _float = lambda x: float(str.lower(x).replace(',', '.'))
 def get_ephemeride():
     data = read_csv('../csvs/Ephemerides1.csv')
     data[:] = map(lambda line: [_float(x) for x in line], data[1:-1])
-    return np.array(data).swapaxes(0, 1)
+    return np.array(data)
 
 
 def get_pseudodist():
